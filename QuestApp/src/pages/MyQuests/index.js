34a5a84a9586371213c1b00c3b8553cc1c2cc7b0
dancey-apple <dancey.apple@proton.me>
2024-08-17 10:56:56 -0500

    Merged with DA-Questline

--- QuestApp/src/pages/MyQuests/index.js
+++ QuestApp/src/pages/MyQuests/index.js
@@ -98,7 +98,6 @@ export default function MyQuest() {
               padding: "10px",
               cursor: "pointer"
             }}
-            onClick={() => router.push(`/quests/${quest.id}`)}
           >
             <h2>{quest.summary}</h2>
             <p>{quest.description}</p>
