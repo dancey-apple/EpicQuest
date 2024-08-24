d9ffcca49008a49b12358c0cd2aadee67756271e
dancey-apple <dancey.apple@proton.me>
2024-08-24 13:03:54 -0500

    added styles to the landing page

--- QuestApp/src/pages/LandingPage/index.js
+++ QuestApp/src/pages/LandingPage/index.js
@@ -9,13 +9,19 @@ const LandingPage = () => {
     }
 
     return (
-        <div className='flex flex-col justify-center ml-28 mt-4'>
-            <div className='flex flex-col  bg-primary rounded-lg'>
-                <h1 className='flex flex-row justify-center '>Adventure Awaits!</h1>
-                <p>Take on quests, earn XP, and level up!</p>
-                <p>Unlock the power of productivity with our gamified app that turns your daily to-dos into epic quests! Earn Experience Points (XP) and Gold as you conquer each task, leveling up your virtual character along the way. Spend your hard-earned Gold on loot, perks, or even PTO, while your XP helps you climb the ranks, boosting your skills and unlocking new rewards. Whether you're grinding through your workday or slaying personal goals, our app makes it fun, engaging, and rewarding. Start your journey today—because productivity should be an adventure!</p>
-                <p>Log in to begin tracking your Quests!</p>
-                <button onClick={(goToNewUser)}>Join the Guild!</button>
+        <div className='flex flex-col justify-center ml-28 mt-4 mr-4'>
+            <div className='flex flex-row justify-center text-secondary font-bold text-6xl hover:drop-shadow-gl mb-8'>Welcome to EpicQuest!</div>
+            <div className='flex flex-col  bg-yellow-900 rounded-lg drop-shadow-gl'>
+                <h1 className='flex flex-row justify-center text-secondary font-bold text-3xl mb-4'>Your Adventure Awaits!</h1>
+                <img className='rounded-lg' src="./img/coverImage.png" alt="EpicQuest Cover Image" />
+                <div className='flex flex-col text-center justify-center text-secondary font-bold gap-4 text-lg'>
+                    <p>Take on quests, earn XP, and level up!</p>
+                    <p>Unlock the power of productivity with our gamified app that turns your daily to-dos into epic quests! Earn Experience Points (XP) and Gold as you conquer each task, leveling up your virtual character along the way. Spend your hard-earned Gold on loot, perks, or even PTO, while your XP helps you climb the ranks, boosting your skills and unlocking new rewards. Whether you're grinding through your workday or slaying personal goals, our app makes it fun, engaging, and rewarding. Start your journey today—because productivity should be an adventure!</p>
+                    <p>Log in to begin tracking your Quests!</p>
+                </div>
+                <div id="button-container" className='flex flex-row justify-center m-4'>
+                    <button className='font-bold hover:text-purple-950 rounded-xl hover:flex-grow bg-secondary text-primary hover:drop-shadow-gl hover:bg-glow p-1 transition-all duration-300' onClick={(goToNewUser)}>Join the Guild!</button>
+                </div>
             </div>
         </div>
     )
--- QuestApp/src/pages/index.js
+++ QuestApp/src/pages/index.js
@@ -7,7 +7,6 @@ export default function BountyBoard() {
   return (
     <main>
       <NavBar />
-      <h1 className='flex flex-row justify-center text-4xl text-secondary hover:text-glow hover:drop-shadow-gl transition-all duration-300 mt-5'>Welcome to Epic Quest!</h1>
       <LandingPage />
     </main>
   );
