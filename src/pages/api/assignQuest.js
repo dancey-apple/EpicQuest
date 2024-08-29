daffc8dc1c9bfbc2efa78b069779676af42969c3
dancey-apple <dancey.apple@proton.me>
2024-08-29 14:24:34 -0500

    prep for turso

--- src/pages/api/assignQuest.js
+++ src/pages/api/assignQuest.js
@@ -1,6 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
-const prisma = new PrismaClient();
+const libsql = createClient({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+})
+
+const adapter = new PrismaLibSQL(libsql)
+const prisma = new PrismaClient({ adapter })
+
+/*const prisma = new PrismaClient(); --OLNY UNCOMMENT IF ON LOCAL*/
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
