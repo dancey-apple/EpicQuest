046b2744a2dc9190344847726be8e4e82661fda0
dancey-apple <dancey.apple@proton.me>
2024-08-30 12:17:15 -0500

    Replaced Turso Doc code for prisma connection

--- src/pages/api/activeQuest.js
+++ src/pages/api/activeQuest.js
@@ -3,14 +3,14 @@ import { PrismaLibSQL } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
 
 const libsql = createClient({
-  url: `${process.env.TURSO_DATABASE_URL}`,
-  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+    url: `${process.env.TURSO_DATABASE_URL}`,
+    authToken: `${process.env.TURSO_AUTH_TOKEN}`,
 })
 
 const adapter = new PrismaLibSQL(libsql)
 const prisma = new PrismaClient({ adapter })
 
-/*const prisma = new PrismaClient(); --OLNY UNCOMMENT IF ON LOCAL*/
+/*const prisma = new PrismaClient();*/
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -2,15 +2,16 @@ import { PrismaClient } from '@prisma/client'
 import { PrismaLibSQL } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
 
-const libsql = createClient({
+/*const libsql = createClient({
     url: `${process.env.TURSO_DATABASE_URL}`,
     authToken: `${process.env.TURSO_AUTH_TOKEN}`,
 })
 
 const adapter = new PrismaLibSQL(libsql)
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient({ adapter })*/
 
-/*const prisma = new PrismaClient(); --OLNY UNCOMMENT IF ON LOCAL*/
+const prisma = new PrismaClient();
+
 
 export default async function handler(req, res) {
     if (req.method === "GET") {
